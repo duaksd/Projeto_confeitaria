b254c9e5747894e693d01fa4ff067a0722319308
duaksd <casado.eduarda@aluno.ifsp.edu.br>
2025-08-21 14:34:28 -0300

    atualização estilo layout inicial

--- src/components/Capa/index.js
+++ src/components/Capa/index.js
@@ -3,8 +3,8 @@ import estilos from './Capa.module.css'
 export default function Capa() {
     return (
         <section className={estilos.secao_capa}>
+             <div className={estilos.capa}>
             <div className={estilos.texto}>
-                <div className={estilos.textobox}>
                 <h2>Sua sobremesa favorita</h2>
                 <h1>AINDA MELHOR</h1>
                 </div>
